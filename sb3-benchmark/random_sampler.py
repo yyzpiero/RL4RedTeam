c1b868d0e21aff49379d730daf34836b59bfd3ca
yyzpiero <yyzpiero@gmail.com>
2022-05-24 11:11:12 +0800

    mm

--- sb3-benchmark/random_sampler.py
+++ sb3-benchmark/random_sampler.py
@@ -1,22 +1,24 @@
 import gym
+from matplotlib.pyplot import step
 import numpy as np
 import time
 import nasim
 
-env = nasim.generate(num_hosts=30, num_services=5, num_os=3, num_processes=2, \
-                    num_exploits=None, num_privescs=None, r_sensitive=10, r_user=10, \
-                    exploit_cost=1, exploit_probs="mixed", privesc_cost=1, privesc_probs=1.0, \
-                    service_scan_cost=1, os_scan_cost=1, subnet_scan_cost=1, process_scan_cost=1,\
-                    uniform=False, alpha_H=2.0, alpha_V=2.0, lambda_V=1.0, restrictiveness=3, \
-                    random_goal=True, base_host_value=1, host_discovery_value=1, \
-                    seed=None, name=None, step_limit=1000000, address_space_bounds=None, yz_gen=True)
-#env = gym.make("nasim:HugeGen-v0")
+env = nasim.generate(num_hosts = 200, 
+                     num_os = 3,
+                     num_services = 10,
+                     num_exploits = 30,
+                     num_processes = 3,
+                     restrictiveness = 5,
+                     step_limit = 300000,
+                    yz_gen=False)
+#env = gym.make("nasim:Pocp2Gen-v0")
 env = gym.wrappers.RecordEpisodeStatistics(env)
 
 action_space_size = env.action_space.n
 
 sps = []
-
+steps = []
 for i in range(10):
     ob = env.reset()
     done = False
@@ -31,11 +33,13 @@ for i in range(10):
 
         if done:
             print("===Done====")
-            print(info)
+            episodic_steps = info['episode']['l']
             break
     time_elapsed = time.time()-since
     sps.append(num_sample/time_elapsed)
+    steps.append(episodic_steps)
 
 print("Aerage Sample per second:{}".format(np.mean(sps)))
 print("Sample per second SD:{}".format(np.std(sps)))
 print("Sample per second :{:.2f} +- {:.2f}".format(np.mean(sps), np.std(sps)))
+print("Steps to solve: {:.1f} +- {:.1f}".format(np.mean(steps), np.std(steps)))
